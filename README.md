ad17b9364c22d6ba6235fc1a6c494015dabd407b
Zachary Smith <zacharysmith2000@googlemail.com>
2018-02-20 19:21:37 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+#  Physics
+
+A series of small applets expanding popular physics topics.
+
+## Contents
+|  |  |
+| :---: | :---: |
+| /blackholes | Web applet for calculating the escape velocity of a customisable star, as well as it's Schwarzschild radius. |
---
+++ blackholes/code.js
@@ -0,0 +1,82 @@
+var gravity = 6.67 * Math.pow(10, -11);
+
+$(document).ready(function() {
+    var SOLOutput = document.getElementById("SOL");
+    SOLOutput.innerHTML = (3 * Math.pow(10, 8)).toExponential();
+
+    var radiusSlider = document.getElementById("radius-slider");
+    var radiusOutput = document.getElementById("radius-value");
+    radiusOutput.innerHTML = radiusSlider.value;
+
+    var massSlider = document.getElementById("mass-slider");
+    var massOutput = document.getElementById("mass-value");
+    massOutput.innerHTML = massSlider.value;
+
+    radiusSlider.oninput = function() {
+        radiusOutput.innerHTML = this.value;
+        $("#sun").height(this.value*2);
+        $("#sun").width(this.value*2);
+        escapeVelocity(massSlider.value, radiusSlider.value);
+    }
+
+    massSlider.oninput = function() {
+        massOutput.innerHTML = this.value;
+        var color = approximateColor1ToColor2ByPercent('#fcd440', '#ff6600', this.value/200);
+        $("#sun").css("background-color", color);
+        escapeVelocity(massSlider.value, radiusSlider.value);
+    }
+
+    escapeVelocity(massSlider.value, radiusSlider.value);
+});
+
+function collapseStar() {
+
+    $("#info").html("&#10687; = photon sphere (outer) and event horizon (inner).");
+
+    $("#constant").hide();
+    $("#radius").hide();
+    $("#mass").hide();
+
+    // schwarzschild radius
+    var mass = document.getElementById("mass-slider").value * (2 * Math.pow(10, 30));
+
+    var division = (2*mass*gravity)/Math.pow(3*Math.pow(10, 8), 2);
+    var radius = division;
+    var schwarzschild = document.getElementById("schwarzschild-value");
+
+    var string = division.toExponential().toString();
+    var split = string.split('e');
+    var first = split[0];
+    var last = split[1];
+    
+    schwarzschild.innerHTML = Math.round(first)+"e"+last;
+
+    escapeVelocity(mass, radius);
+
+    $("#sun").css("background-color", "#fff");
+    $("#sun").animate({
+        height: "2px",
+        width: "2px",
+    }, 1000, function() {
+        // add photon sphere
+        $("#sun").css("border-style", "double");
+    });
+}
+
+function escapeVelocity(mass_value, radius_value) {
+    
+    var radius = radius_value * (6.597 * Math.pow(10, 8));
+    var mass = mass_value * (2 * Math.pow(10, 30));
+
+    var division = (2*mass*gravity)/radius;
+    var root = Math.sqrt(division);
+
+    var escapeVelocity = document.getElementById("velocity-value");
+
+    var string = root.toExponential().toString();
+    var split = string.split('e');
+    var first = split[0];
+    var last = split[1];
+
+    escapeVelocity.innerHTML = Math.round(first)+"e"+last;
+}
